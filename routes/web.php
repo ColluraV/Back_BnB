865fae2eecc206e11b6150927267e9eb20b186d9
Valerio <valerio.collura@live.it>
2024-01-30 17:00:25 +0100

    addedd route on web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\ApartmentController;
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\VisitController;
+use Illuminate\Routing\ViewController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -28,4 +32,17 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+
+Route::middleware('auth')
+    ->prefix("admin") //Path prefix
+    ->name("admin.") //Name prefix
+    ->group(function () {
+        Route::resource("apartments", ApartmentController::class);
+        Route::get("messages", [MessageController::class, "index"])->name("messages");
+        Route::get("statistics/{id}", [VisitController::class, "show"])->name("statistics.show");
+    });
+
+
 require __DIR__.'/auth.php';
+
+
