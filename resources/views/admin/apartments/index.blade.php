1b99b488dd7299f5eadc22719f51ed6d92b2a255
Valerio <valerio.collura@live.it>
2024-02-01 15:24:30 +0100

    create pages

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,7 +1,38 @@
 @extends('layouts.app')
 @section('content')
+<div class="container">
 <h2>Apartments test</h2>
-    @dump($apartments)
+    @foreach ($apartments as $apartment)
+        <div class="d-flex justify-content-center">
+        <ul>
+            <h3>
+                {{$apartment->title}}
+            </h3>
+            <li>
+                indirizzo: {{$apartment->address}}
+            </li>
+            <li>
+                num bagni: {{$apartment->bath_number}}
+            </li>
+            <li>
+                num letti: {{$apartment->beds_number}}
+            </li>
+            <li>
+                metratura: {{$apartment->dimensions}}mq
+            </li>
+            <li>
+                lat: {{$apartment->latitude}}
+            </li>
+            <li>
+                long: {{$apartment->longitude}}
+            </li>
+        </ul>
+    </div>
+    
+        
+    @endforeach
+</div>
+<style>
 
-
+</style>
 @endsection
