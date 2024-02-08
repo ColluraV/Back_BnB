a163934d702883f32dd6dd0e8e6c03e85d5a0141
MirkoGLostia <giggi.torre@mail.com>
2024-02-08 16:15:29 +0100

    comments to api and tomtomController

--- app/Http/Controllers/Api/TomtomController.php
+++ app/Http/Controllers/Api/TomtomController.php
@@ -7,13 +7,16 @@ use Illuminate\Http\Request;
 
 class TomtomController extends Controller
 {
+    // Initialization of constants
     private $baseUrl = 'https://api.tomtom.com/search/2/geocode/';
     private $apiKey = '';
 
+    // teoricamente dovrebbe prendere il valore dal file env e darlo a $apiKey
     public function _construct() {
         $this -> apiKey = env('api_key');
     }
 
+    // test per vedere se è connesso con le rotte api
     public function test(Request $request) {
         $response = $request -> input('location');
         return response()->json($response);
@@ -22,22 +25,27 @@ class TomtomController extends Controller
     public function getGeoData(Request $request)
     {
 
+        // take the input value and put it into the variable $location
         $location = $request -> input('location');
         
-        /*$response = Http::withOptions(['verify' => false])->get("$this->baseUrl/$location.json", [
+        // teoricamente dovrebbe costruire il link a tomtom, ma dato che non prende l'apiKey, non riesce neanche a connettersi
+        $response = Http::withOptions(['verify' => false])->get("$this->baseUrl/$location.json", [
             "storeResult" => false,
             "limit" => 10,
             "countrySet" => "it",
             "key" => $this->apiKey
-        ]);*/
+        ]);
 
-        $response = "$this->baseUrl/$location.json?key=$this->apiKey"; 
+        // test per vedere il link che costruisce
+        // $response = "$this->baseUrl/$location.json?key=$this->apiKey"; 
 
-        return $response;
+        // return per i test
+        // return $response;
 
+        // variable filled with the locations 'cleaned' from nnecessary data
         $preparedLocations = $this->prepareLocations($response->json('results'));
 
-        // return response()->json($preparedLocations);
+        return response()->json($preparedLocations);
     }
 
     private function prepareLocations($locations) 
--- routes/api.php
+++ routes/api.php
@@ -19,5 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
+// route to link to the tomtom controller
 Route::get('/address', [TomtomController::class, 'getGeoData'])->name('getAddress');
